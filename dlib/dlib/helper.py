bca5c22592238b50a7b65ba764d14fc05e51d502
Jackson Rudd <jackson.h.rudd@gmail.com>
2024-11-28 11:10:50 -0800

    better

--- dlib/dlib/helper.py
+++ dlib/dlib/helper.py
@@ -2,51 +2,6 @@ from IPython.display import Markdown, display
 def printmd(*args):
     display(Markdown(' '.join(args)))
 
-import matplotlib.pyplot as plt
-
-
-class VectorScene():
-    def __init__(self):
-        plt.style.use('dark_background')
-        self.fig, self.ax = plt.subplots()
-        self.vectors = []
-    
-    def add_vector(self, vector, color='b', label='vector'):
-        # if we have a tf vector, convert it to numpy
-        if hasattr(vector, 'numpy'):
-            vector = vector.numpy()
-        self.add_vector_at([0,0], vector, color, label)
-        return self
-
-    def add_vector_at(self, start_vector, delta_vector, color='b', name='vector'):
-        # if we have a tf vector, convert it to numpy
-        if hasattr(start_vector, 'numpy'):
-            start_vector = start_vector.numpy()
-        if hasattr(delta_vector, 'numpy'):
-            delta_vector = delta_vector.numpy()
-        self.vectors.append((start_vector, delta_vector, color, name))
-        return self
-        
-
-    def display(self):
-        self.ax.set_aspect('equal')
-        for start_vector, delta_vector, color, label in self.vectors:
-            self.ax.quiver(start_vector[0], start_vector[1], delta_vector[0], delta_vector[1], angles='xy', scale_units='xy', scale=1, color=color, label=label)
-        x_max = max([v[0][0]+v[1][0] for v in self.vectors])
-        y_max = max([v[0][1]+v[1][1] for v in self.vectors])
-        x_min = min([v[0][0] for v in self.vectors])
-        y_min = min([v[0][1] for v in self.vectors])
-        largest = max(x_max, x_min, y_max, y_min)
-        
-        self.ax.set_xlim([-1.2*largest, 1.2*largest])
-        self.ax.set_ylim([-1.2*largest, 1.2*largest])
-        # add a x and y axis
-        plt.grid()
-        plt.legend(loc='upper left', bbox_to_anchor=(1, 1))
-        plt.show()
-
-
-from matplotlib.animation import FuncAnimation
 
 import matplotlib.pyplot as plt
 from matplotlib.animation import FuncAnimation
@@ -74,28 +29,32 @@ class AnimatedVector:
         self.color = color
         self.name = name
 
+class StaticVector:
+    def __init__(self, start_position: Vector, delta: Vector, color: str = 'w', name: str = 'static_vector'):
+        self.start_position = start_position
+        self.delta = delta
+        self.color = color
+        self.name = name
 
-class AnimatedVectorScene:
-    def __init__(self):
+
+class SceneMaker:
+
+    def setup(self, animated_vectors: List[AnimatedVector], static_vectors: List[StaticVector]) -> None:
+        pass
+
+    def animate(self, name: str) -> None:
+        pass
+
+    def display(self):
+        pass
+
+class MatplotlibScene(SceneMaker):
+
+    def setup(self, animated_vectors: List[AnimatedVector], static_vectors: List[StaticVector]) -> None:
+        self.animated_vectors = animated_vectors
+        self.static_vectors = static_vectors
         plt.style.use('dark_background')
         self.fig, self.ax = plt.subplots()
-        self.animated_vectors: List[AnimatedVector] = []  # Store AnimatedVector objects
-
-    @staticmethod
-    def _convert_to_vectors(vector_list: List[Union[List[float], Tuple[float, float]]]) -> List[Vector]:
-        return [Vector(v[0], v[1]) for v in vector_list]
-
-    def add_animated_vector(self, deltas: List[Union[List[float], Tuple[float, float]]], color: str = 'b', name: str = 'vector') -> 'AnimatedVectorScene':
-        start_positions = [[0, 0] for _ in deltas]
-        return self.add_animated_vector_at(start_positions, deltas, color, name)
-
-    def add_animated_vector_at(self, start_positions: List[Union[List[float], Tuple[float, float]]], deltas: List[Union[List[float], Tuple[float, float]]], color: str = 'b', name: str = 'vector') -> 'AnimatedVectorScene':
-        start_positions = self._convert_to_vectors(start_positions)
-        deltas = self._convert_to_vectors(deltas)
-        self.animated_vectors.append(AnimatedVector(start_positions, deltas, color, name))
-        return self
-
-    def _set_scene(self):
         self.ax.set_aspect('equal')
         largest = 0
 
@@ -108,6 +67,14 @@ class AnimatedVectorScene:
                     abs(end.x), abs(end.y),
                     abs(start.x), abs(start.y)
                 )
+        
+        for vector in self.static_vectors:
+            end = vector.start_position + vector.delta
+            largest = max(
+                largest,
+                abs(end.x), abs(end.y),
+                abs(vector.start_position.x), abs(vector.start_position.y)
+            )
 
         self.ax.set_xlim([-1.2 * largest, 1.2 * largest])
         self.ax.set_ylim([-1.2 * largest, 1.2 * largest])
@@ -115,7 +82,16 @@ class AnimatedVectorScene:
 
         quivers = []  # To store quiver objects for updating in animation
 
-        # Initialize quivers
+        # Initialize static vectors
+        for vector in self.static_vectors:
+            self.ax.quiver(
+                vector.start_position.x, vector.start_position.y,
+                vector.delta.x, vector.delta.y,
+                angles='xy', scale_units='xy', scale=1,
+                color=vector.color, label=vector.name
+            )
+
+        # Initialize animated vectors
         for vector in self.animated_vectors:
             quiver = self.ax.quiver(
                 [vec.x for vec in vector.start_positions],
@@ -137,14 +113,63 @@ class AnimatedVectorScene:
                 quiver.set_UVC(delta.x, delta.y)  # Update vector direction and magnitude
             return quivers
 
-        return update
+        self.update_fn = update
 
     def display(self):
-        update = self._set_scene()
         plt.show()
 
-    def animate(self, name: str, interval: int = 100):
+    def animate(self, name: str):
+
         # Create animation
         frames = len(self.animated_vectors[0].start_positions)  # Assume all vectors have the same number of frames
-        anim = FuncAnimation(self.fig, self._set_scene(), frames=frames, interval=interval, blit=True)
+        anim = FuncAnimation(self.fig, self.update_fn, frames=frames, interval=100, blit=True)
         anim.save(f'video/{name}.gif', writer='imagemagick', fps=5)
+
+
+class VectorScene():
+    def __init__(self, scene_maker: SceneMaker= None):
+        self.setup_called = False
+        self.animated_vectors: List[AnimatedVector] = []  # Store AnimatedVector objects
+        self.static_vectors: List[StaticVector] = []  # Store StaticVector objects
+        self.scene_maker = scene_maker
+        if scene_maker is None:
+            self.scene_maker = MatplotlibScene()
+
+    @staticmethod
+    def _convert_to_vectors(vector_list: List[Union[List[float], Tuple[float, float]]]) -> List[Vector]:
+        return [Vector(v[0], v[1]) for v in vector_list]
+
+    def add_animated_vector(self, deltas: List[Union[List[float], Tuple[float, float]]], color: str = 'b', name: str = 'vector') -> 'AnimatedVectorScene':
+        start_positions = [[0, 0] for _ in deltas]
+        return self.add_animated_vector_at(start_positions, deltas, color, name)
+
+    def add_animated_vector_at(self, start_positions: List[Union[List[float], Tuple[float, float]]], deltas: List[Union[List[float], Tuple[float, float]]], color: str = 'b', name: str = 'vector') -> 'AnimatedVectorScene':
+        start_positions = self._convert_to_vectors(start_positions)
+        deltas = self._convert_to_vectors(deltas)
+        self.animated_vectors.append(AnimatedVector(start_positions, deltas, color, name))
+        return self
+
+    def add_vector(self, delta: Union[List[float], Tuple[float, float]], color: str = 'w', name: str = 'static_vector') -> 'AnimatedVectorScene':
+        return self.add_vector_at([0, 0], delta, color, name)
+    
+    def add_vector_at(self, start_position: Union[List[float], Tuple[float, float]], delta: Union[List[float], Tuple[float, float]], color: str = 'w', name: str = 'static_vector') -> 'AnimatedVectorScene':
+        start_position = Vector(*start_position)
+        delta = Vector(*delta)
+        self.static_vectors.append(StaticVector(start_position, delta, color, name))
+        return self
+    
+    def setup(self):
+        if self.setup_called:
+            return
+        self.setup_called = True
+
+        self.scene_maker.setup(self.animated_vectors, self.static_vectors)
+
+    def animate(self, name: str):
+        self.setup()
+        self.scene_maker.animate(name)
+
+    def display(self):
+        self.setup()
+        self.scene_maker.display()
+    
